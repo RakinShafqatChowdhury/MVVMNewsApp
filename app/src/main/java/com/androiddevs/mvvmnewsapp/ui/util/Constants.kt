9fcc5eecedc1c2f73306be795643e8ff1dc16b1c
rakin <rakin.cel@gmail.com>
2022-10-30 10:15:50 +0600

    Initial commit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "82f88fa778514795949eb5b2d0a69243"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/viewmodels/NewsViewModel.kt
@@ -0,0 +1,32 @@
+package com.androiddevs.mvvmnewsapp.ui.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.ui.model.NewsResponse
+import com.androiddevs.mvvmnewsapp.ui.repository.NewsRepository
+import com.androiddevs.mvvmnewsapp.ui.util.Resource
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class NewsViewModel(
+    val newsRepository: NewsRepository
+) : ViewModel() {
+
+    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNewsPage = 1
+
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
+        breakingNews.postValue(Resource.Loading())
+        val response = newsRepository.getBreakingNews(countryCode, breakingNewsPage)
+    }
+
+    private fun handleBreakingNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let {
+                return Resource.Success(it)
+            }
+        }
+        return Resource.Error(response.message())
+    }
+}
